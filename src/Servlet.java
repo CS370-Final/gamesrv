aa8bde8889feb61be680b8e207b75662d772653b
Richard Lomax <fishbone@BRASHVILLE.ORG>
2020-05-22 01:05:39 -0400

    Created wrapper servlet class for servlet requests to database

--- src/Servlet.java
+++ src/Servlet.java
@@ -1,50 +1,19 @@
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import org.httprpc.io.JSONEncoder;
 import org.httprpc.sql.ResultSetAdapter;
 
-/**
- * Servlet implementation class Servlet
- */
-@WebServlet("/Servlet")
-@MultipartConfig()
-public class Servlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public Servlet() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DBconn db=null;
-		ResultSetAdapter rs=null;
+public class Servlet {
+	private static DBconn db=DBconn.makeConn();
+	private static ResultSetAdapter rs;
+	private static JSONEncoder json=new JSONEncoder();
+	
+	public static void out(String query,PrintWriter writer) {
+		Servlet.rs=new ResultSetAdapter(Servlet.db.query(query));
 		try {
-			db=DBconn.makeConn();
-			rs=new ResultSetAdapter(db.query("Select * FROM users"));
-			JSONEncoder json=new JSONEncoder();
-			PrintWriter res=response.getWriter();
-			response.setContentType("text/html");
-			if(rs!=null) {
-				json.write(rs,res);
-			}
-		} catch (SQLException | ClassNotFoundException e) {
+			Servlet.json.write(rs,writer);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
